cf53b513a47e8c1121270951bb15b64c1cd6f0af
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-04-26 20:50:06 -0700

    Update requests_get_example_01.py

--- requests_get_example_01.py
+++ requests_get_example_01.py
@@ -1,6 +1,7 @@
 import requests
 #first parameter of the 'get' method is the 'url':
-x = requests.get('https://w3schools.com/python/demopage.htm')
+url = 'https://w3schools.com/python/demopage.htm'
+x = requests.get(url)
 
 #print the response text (the content of the requested file):
 print(x.text)
