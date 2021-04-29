4c47e3c365147a4e08d782fad259a10b8b7d3496
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-04-28 20:50:20 -0700

    Create requests_get_example_03.py

---
+++ requests_get_example_03.py
@@ -0,0 +1,7 @@
+import requests
+r = requests.get('https://www.steamclown.org/projects/QInlIj_vIHev/Huch_QIn/all_robots_command_requests.txt')
+
+whole_file = r.text
+print(whole_file)
+whole_file = r.content
+print(whole_file)
