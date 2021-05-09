fe5c3306124b6473c9c6e497968d775845ed5843
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-05-09 09:48:50 -0700

    Update openGoogleMapsWithAddress.py

--- openGoogleMapsWithAddress.py
+++ openGoogleMapsWithAddress.py
@@ -3,7 +3,7 @@ import sys
 try:
     if len(sys.argv) > 1 :
         # get argument string from command line
-        address = ''.join(sys.argv[1:])
-    webbrowser.open('https://www.google.com/maps/place/' + address)
+        cmd_parameter = ''.join(sys.argv[1:])
+    webbrowser.open('https://www.google.com/maps/place/' + cmd_parameter)
 except:
     print('no address provided') 
