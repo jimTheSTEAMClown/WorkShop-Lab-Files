ea6f24dc197f21d4374d98c523909be88e4bad8d
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-05-09 09:48:25 -0700

    Create pass_commandline_parameter.py

---
+++ pass_commandline_parameter.py
@@ -0,0 +1,9 @@
+import webbrowser
+import sys
+try:
+    if len(sys.argv) > 1 :
+        # get argument string from command line
+        cmd_parameter = ''.join(sys.argv[1:])
+    print(cmd_parameter)
+except:
+    print('no address provided') 
