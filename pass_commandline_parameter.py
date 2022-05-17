60ffb379a77a1c75b3e9ea824903ecf35ced28c1
Jim Burnham <jim.the.steam.clown@gmail.com>
2022-05-16 18:03:19 -0700

    Update pass_commandline_parameter.py

--- pass_commandline_parameter.py
+++ pass_commandline_parameter.py
@@ -3,7 +3,7 @@ import sys
 try:
     if len(sys.argv) > 1 :
         # get argument string from command line
-        cmd_parameter = ''.join(sys.argv[1:])
-    print(cmd_parameter)
+        cmd_parameter1 = ''.join(sys.argv[1:])
+    print(cmd_parameter1)
 except:
     print('no cmd argument provided') 
